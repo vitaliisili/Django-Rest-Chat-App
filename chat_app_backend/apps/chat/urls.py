726e548af1b2680e2a24c4a724a06fb9d24cd790
Vitalii Sili <vitaliisili@yahoo.com>
2024-01-29 23:28:21 +0100

    add websocket url room name

--- chat_app_backend/apps/chat/urls.py
+++ chat_app_backend/apps/chat/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-
 from apps.chat import views
 
 router = routers.DefaultRouter()
-router.register('chat-requests', views.ChatRequestViewSet, basename='chat-requests')
+router.register('chat-requests', views.ChatRequestViewSet, basename='chat-requests'),
+router.register('chat-rooms', views.ChatRoomViewSet, basename='chat-rooms'),
 
 urlpatterns = [
     path('', include(router.urls))
--- chat_app_backend/config/urls.py
+++ chat_app_backend/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 
@@ -23,3 +25,5 @@ urlpatterns = [
     path('api/accounts/', include('apps.accounts.urls')),
     path('api/chat/', include('apps.chat.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
